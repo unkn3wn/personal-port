fc954fc7d25358b8239d8d1bc098efae41efbf8b
unkn3wn <reyesfernando151@gmail.com>
2022-12-12 22:00:53 -0800

    restart of portfolio

---
+++ src/App.jsx
@@ -0,0 +1,43 @@
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import { useState, useEffect } from "react";
+
+import Home from "./components/Home";
+
+function App() {
+  const [theme, setTheme] = useState("light");
+  
+
+  useEffect(() => {
+    if (theme === "dark") {
+      document.documentElement.classList.add("dark");
+    } else {
+      document.documentElement.classList.remove("dark");
+    }
+  }, [theme]);
+
+
+
+  const handleThemeSwitch = () => {
+    setTheme(theme === "dark" ? "light" : "dark");
+  };
+
+
+  return (
+    <div className="h-screen white:bg-white dark:bg-black dark:text-white">
+      <button
+        className="bg-green-200 p-4 rounded-3xl"
+        onClick={() => {
+          handleThemeSwitch();
+        }}
+      >
+        Dark Mode
+      </button>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Home.jsx
@@ -0,0 +1,27 @@
+import react from "react";
+import styles from "../styles/home.module.css";
+
+
+
+export default function Home() {
+  return (  
+    <div>
+      
+      <section>
+      <h1>this is cool</h1>
+      </section>
+      <section>
+        {/* pg two */}
+      </section>
+      <section>
+        {/* pg three */}
+      </section>
+      <section>
+      </section>
+
+
+
+
+    </div>
+  );
+}
